228b379d1806a645a1b534e96c5650710facfd73
zeiadsalhin <zeyadali011@gmail.com>
2024-07-11 14:19:29 +0300

    v1

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -52,26 +52,12 @@ export default defineNuxtConfig({
       cookieRedirect: false,
     }
   },
-  // stripe: {
-  //   // Server
-  //   // server: {
-  //   //   key: "sk_test_123",
-  //   //   options: {
-  //   //     // your api options override for stripe server side
-  //   //     // https://github.com/stripe/stripe-node?tab=readme-ov-file#configuration
-  //   //   },
-  //   //   // CLIENT
-  //   // },
-  //   client: {
-  //     key: "pk_test_51PbCugI4aHqhinrA9sCytQD7jIlhqLWBYQireLPZjFtcIEBs5Sgxt46yDomCTz6xHjnloKIJIveFxbgmNjZ9f0la00uY4BdDvv",
-  //     // your api options override for stripe client side https://stripe.com/docs/js/initializing#init_stripe_js-options
-  //     options: {},
-  //   },
-  // },
   runtimeConfig: {
     public: {
-      appUrl: 'http://localhost:3000',
-      stripePk: 'pk_test_51P5QowHndLqqnsBRVpdsfH0OUfKfxl5fNWNlK8wtaSgbKOmbWzYeFZxX9KKFcDAtAX1J4gDcjjh1Eq8R58ZjkI2W00QPdoeNpJ',
+      appUrl: process.env.NODE_ENV === 'production'
+      ? 'https://chattokgaming.com/'
+      : 'http://localhost:3000/',
+      stripePk: import.meta.env.VITE_STRIPE_PUBLIC_KEY,
     },
   }
 })
