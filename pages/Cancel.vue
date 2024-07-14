b7deab8531294b0656a4a3b6b53ade25e522e6a9
zeiadsalhin <zeyadali011@gmail.com>
2024-07-14 04:52:39 +0300

    v1

---
+++ pages/Cancel.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <div class="fixed left-0 top-0 h-full w-full bg-zinc-950">
+            <div class="flex justify-center items-center h-full">
+                <div class="flex flex-col items-center">
+                    <Vue3Lottie animationLink="/Canceled.json" :loop="1" :height="200" :width="200" />
+                    <!-- <v-icon icon="mdi-check-circle" color="green" class="m-5" style="font-size: 4rem;"></v-icon> -->
+                    <h1 class="p-2 text-xl font-semibold">You canceled your order</h1>
+                    <p class="text-sm">Redirecting in {{ countdown }}</p>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { useMainStore } from '~/store';
+const router = useRouter()
+const mainStore = useMainStore();
+
+const countdown = ref(5);
+onMounted(() => {
+    const interval = setInterval(() => {
+        countdown.value--;
+        if (countdown.value === 0) {
+            clearInterval(interval);
+            navigateTo('/user/account')
+        }
+    }, 1000);
+});
+</script>
--- pages/success.vue
+++ pages/success.vue
@@ -1,8 +1,17 @@
 <template>
     <div>
-        <h1>Payment Successful</h1>
-        <p>Your payment was successful.</p>
-        <p>Redirecting in {{ countdown }}</p>
+        <div class="fixed left-0 top-0 h-full w-full bg-zinc-950">
+            <div class="flex justify-center items-center h-full">
+                <div class="flex flex-col items-center">
+                    <Vue3Lottie animationLink="/completed.json" :loop="1" :height="200" :width="200" />
+                    <!-- <v-icon icon="mdi-check-circle" color="green" class="m-5" style="font-size: 4rem;"></v-icon> -->
+                    <h1 class="p-2 text-xl font-semibold">Order Completed !</h1>
+                    <p class="p-2">Your payment was successful.</p>
+                    <p class="text-sm">Redirecting in {{ countdown }}</p>
+                </div>
+            </div>
+
+        </div>
     </div>
 </template>
 
