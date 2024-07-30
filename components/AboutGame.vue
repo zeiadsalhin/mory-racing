10bdd41d688166497ba8ac5c42c233d8aa96d163
zeiadsalhin <zeyadali011@gmail.com>
2024-07-31 00:39:54 +0300

    v1

--- components/AboutGame.vue
+++ components/AboutGame.vue
@@ -39,13 +39,14 @@
                             <v-icon class="mx-3 my-auto" size="30">mdi-copyright</v-icon>
                             <h1 class="my-auto"> Credits</h1>
                         </div>
-                        <ul class="mt-5 space-y-5 md:text-2xl text-sm list-[square] p-3 ml-10">
-                            <li class="flex"><v-avatar size="30"><v-img alt="Mory" src="/icon.ico"></v-img></v-avatar>
+                        <ul class="mt-5 space-y-5a md:text-2xl text-sm list-[square] p-3 ml-10">
+                            <li class="flex mb-2"><v-avatar size="30"><v-img alt="Mory"
+                                        src="/icon.ico"></v-img></v-avatar>
                                 <p class="ml-3 my-auto">Mory</p>
                                 <div class="w-5 h-0.5 bg-zinc-800 my-auto ml-4 mr-1"></div>
                                 <p class="md:text-[1.2rem] my-auto brightness-75">Chattok game developer</p>
                             </li>
-                            <p class="text-sm font-thin italic ml-5 opacity-80">"Behind the scenes, our team has
+                            <p class="text-sm font-thin italic ml-5 mb-10 opacity-80">"Behind the scenes, our team has
                                 meticulously
                                 crafted every
                                 aspect
@@ -56,20 +57,45 @@
                                 advanced physics engines and cutting-edge visual effects to push the boundaries of
                                 realism and
                                 excitement."</p>
-                            <li class="flex"><v-avatar size="30"><v-img alt="Mory" src="/b.webp"></v-img></v-avatar>
+                            <li class="flex mb-2"><v-avatar size="30"><v-img alt="Mory"
+                                        src="/b.webp"></v-img></v-avatar>
                                 <p class="ml-3 my-auto">Alfa</p>
                                 <div class="w-5 h-0.5 bg-zinc-800 my-auto ml-4 mr-1"></div>
                                 <p class="md:text-[1.2rem] my-auto brightness-75">Chattok web developer</p>
                             </li>
-                            <p class="text-sm font-thin italic ml-5 opacity-80">"As web developers, we've optimized our
+                            <p class="text-sm font-thin italic ml-5 mb-10 opacity-80">"As web developers, we've
+                                optimized our
                                 Unity
-                                car racing game for performance and accessibility on the web. Leveraging WebGL for
+                                games for performance and accessibility on the web. Leveraging WebGL for
                                 rendering,
                                 HTML5 for interface elements and latest progressive web technologies, we ensure smooth
                                 gameplay
                                 experiences
                                 across various browsers and
                                 devices."</p>
+                            <li class="flex mb-2"><v-avatar size="30"><v-img alt="Mory"
+                                        src="/icon.ico"></v-img></v-avatar>
+                                <p class="ml-3 my-auto">Clinton Deig</p>
+                                <div class="w-5 h-0.5 bg-zinc-800 my-auto ml-4 mr-1"></div>
+                                <p class="md:text-[1.2rem] my-auto brightness-75">Chattok Owner</p>
+                            </li>
+                            <p class="text-sm font-thin italic ml-5 mb-10 opacity-80">"Our TikTok Live games thrives
+                                thanks to the innovative spirit of its platform
+                                team. Their dedication to fostering creativity and engaging experiences has set the
+                                stage for thrilling competitions and endless fun. Here's to pushing the limits and
+                                racing towards a brighter future!."</p>
+                            <li class="flex mb-2"><v-avatar size="30"><v-img alt="Mory"
+                                        src="/icon.ico"></v-img></v-avatar>
+                                <p class="ml-3 my-auto">Jose</p>
+                                <div class="w-5 h-0.5 bg-zinc-800 my-auto ml-4 mr-1"></div>
+                                <p class="md:text-[1.2rem] my-auto brightness-75">Chattok Backdoor</p>
+                            </li>
+                            <p class="text-sm font-thin italic ml-5 mb-10 opacity-80">"Behind every smooth race and
+                                seamless
+                                experience in our TikTok Live games is the
+                                expertise of our backend developer. My dedication to crafting robust systems and
+                                optimizing performance ensures that every race runs like clockwork. A heartfelt thanks
+                                to the tech mastermind making it all possible!"</p>
                             <!-- <li>Project Management</li> -->
                         </ul>
                     </v-img>
