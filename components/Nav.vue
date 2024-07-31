ed229a67588e720dfbbe0ff51ee89a976ea1bb86
zeiadsalhin <zeyadali011@gmail.com>
2024-08-01 02:01:46 +0300

    v1

--- components/Nav.vue
+++ components/Nav.vue
@@ -130,6 +130,26 @@ watch(user, () => {
                         class="m-5" width="50" height="50" alt="logo"></v-img>
                 </v-toolbar-title>
             </nuxt-link>
+            <v-btn :icon="!$vuetify.display.mobile ? false : true"
+                @click="navigateTo('https://www.tiktok.com/@chattoklive', { external: true })"
+                class="mr-2 text-subtitle-2 opacity-85" variant="text">
+                {{ !$vuetify.display.mobile ? 'Follow' : '' }}
+                <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor"
+                    class="bi bi-tiktok m-1" viewBox="0 0 16 16">
+                    <path
+                        d="M9 0h1.98c.144.715.54 1.617 1.235 2.512C12.895 3.389 13.797 4 15 4v2c-1.753 0-3.07-.814-4-1.829V11a5 5 0 1 1-5-5v2a3 3 0 1 0 3 3z" />
+                </svg>
+            </v-btn>
+            <v-btn :icon="!$vuetify.display.mobile ? false : true"
+                @click="navigateTo('https://discord.gg/GywCaY898e', { external: true })"
+                class="mr-2 text-subtitle-2 opacity-85" variant="text">
+                {{ !$vuetify.display.mobile ? 'Join our' : '' }}
+                <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor"
+                    class="bi bi-discord m-1" viewBox="0 0 16 16">
+                    <path
+                        d="M13.545 2.907a13.2 13.2 0 0 0-3.257-1.011.05.05 0 0 0-.052.025c-.141.25-.297.577-.406.833a12.2 12.2 0 0 0-3.658 0 8 8 0 0 0-.412-.833.05.05 0 0 0-.052-.025c-1.125.194-2.22.534-3.257 1.011a.04.04 0 0 0-.021.018C.356 6.024-.213 9.047.066 12.032q.003.022.021.037a13.3 13.3 0 0 0 3.995 2.02.05.05 0 0 0 .056-.019q.463-.63.818-1.329a.05.05 0 0 0-.01-.059l-.018-.011a9 9 0 0 1-1.248-.595.05.05 0 0 1-.02-.066l.015-.019q.127-.095.248-.195a.05.05 0 0 1 .051-.007c2.619 1.196 5.454 1.196 8.041 0a.05.05 0 0 1 .053.007q.121.1.248.195a.05.05 0 0 1-.004.085 8 8 0 0 1-1.249.594.05.05 0 0 0-.03.03.05.05 0 0 0 .003.041c.24.465.515.909.817 1.329a.05.05 0 0 0 .056.019 13.2 13.2 0 0 0 4.001-2.02.05.05 0 0 0 .021-.037c.334-3.451-.559-6.449-2.366-9.106a.03.03 0 0 0-.02-.019m-8.198 7.307c-.789 0-1.438-.724-1.438-1.612s.637-1.613 1.438-1.613c.807 0 1.45.73 1.438 1.613 0 .888-.637 1.612-1.438 1.612m5.316 0c-.788 0-1.438-.724-1.438-1.612s.637-1.613 1.438-1.613c.807 0 1.451.73 1.438 1.613 0 .888-.631 1.612-1.438 1.612" />
+                </svg>
+            </v-btn>
             <v-spacer /> <v-spacer /> <v-spacer /> <v-spacer /> <v-spacer /> <v-spacer /> <v-spacer /> <v-spacer />
             <v-spacer />
             <v-spacer /> <v-spacer /> <v-spacer /> <v-spacer /> <v-spacer /> <v-spacer /> <v-spacer /> <v-spacer />
