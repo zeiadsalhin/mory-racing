54004e9dbd6ea7961d781d02a0fbe8320d9cb924
zeiadsalhin <zeyadali011@gmail.com>
2024-08-08 18:16:06 +0300

    v1

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  sourcemap: true,
   app: {
     head: {
       charset: 'utf-8',
